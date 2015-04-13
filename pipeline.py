e9ea0ef5677f018357c6f18b7b89ba88a6dca954
Dario Cvijanovic <cvijand@fang.cs.sunyit.edu>
2015-04-13 00:25:58 -0400

    Register and memory classes done

---
+++ pipeline.py
@@ -0,0 +1,4 @@
+from hw import register
+
+
+stack_pointer=register(stack_pointer)
---
+++ read_file.py
@@ -0,0 +1,17 @@
+#For reading source code file and turning into
+#executable instructions.
+#NEEDS TO FLAG HAZARDS
+
+# 1 read file line by line.  Copy source from work
+
+
+# 2 convert instructions into 32bit code
+
+
+# 3 check for hazards
+
+
+
+
+
+
