89be91c0c56413c78197a27f075b5aff04ecef5b
Dario Cvijanovic <cvijand@fang.cs.sunyit.edu>
2015-04-14 10:54:04 -0400

    Folder for reading source file

---
+++ read_source/read_file.py
@@ -0,0 +1,17 @@
+#For reading source code file and turning into
+#executable instructions.
+#NEEDS TO FLAG HAZARDS
+
+# 1 read file line by line.  
+
+
+# 2 convert instructions into 32bit code
+
+
+# 3 check for hazards
+
+
+
+
+
+
