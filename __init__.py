45036b80907f8515a8dcb99432c9fddce3bc79fd
Dario Cvijanovic <cvijand@fang.cs.sunyit.edu>
2015-04-14 12:45:52 -0400

    Initialize registers and memory

--- __init__.py
+++ __init__.py
@@ -1,2 +1,26 @@
-#Intentionally left blank for now
-#Needed for old v of python to work
+from hw import register
+from hw import mem_collection
+
+NUM_OF_REG=32
+SIZE_OF_INST_MEM=2048
+SIZE_OF_DATA_MEM=32768
+
+if __name__ == "__main__":
+    #create registers
+    data_reg=[]
+
+    for it in range (0,NUM_OF_REG):
+        name="r{0}".format(it)
+        data_reg.append(register(name))
+
+    #for ti in range(len(data_reg)):
+        #print "Index = {0}".format(ti)
+        #print data_reg[ti].get_name()
+        #print ti
+        
+    #create memory
+    inst_mem=mem_collection("inst", SIZE_OF_INST_MEM)
+    data_mem=mem_collection("data", SIZE_OF_DATA_MEM)
+
+
+    print "Done initializing mem and reg"
--- hw.py
+++ hw.py
@@ -1,30 +1,33 @@
 # Define all the hardware classes
 
+#32-bit strings for initializing and testing
+ALL_ZEROS="00000000000000000000000000000000"
+ALT_FROM_1="10101010101010101010101010101010"
+ALT_FROM_0="01010101010101010101010101010101"
 
 class register:
     data="0"*32
     def __init__(self, name):
         self.name = name
 
+    def write(self, data):
+        self.data=data
 
+    def read(self):
+        return self.data
 
-# IDT this class is necessary
-#class mem_addr:
-#    data="0"*32
-#    def __init__(self, addr):
-#        self.addr=addr
-
-
+    def get_name(self):
+        return self.name
 
 class mem_collection:
     def __init__(self, name, size):
         self.name=name
-        self.mem = []
-        for index in range(size):
-            mem[index]=mem_addr(index)
+        self.mem = [ALL_ZEROS]*size
+        #for index in range(size):
+            #self.mem[index]=ALL_ZEROS
 
-    def load(addr):
+    def load(self, addr):
         return self.mem[addr]
 
-    def save(addr, value):
+    def save(self, addr, value):
         self.mem[addr]=value
