fb7b3a4cb4de1ce4dac4edc65c8d2c4c4cbab15a
Lucas Halbert <halberl@cs.sunyit.edu>
2015-04-15 20:09:06 -0400

    Add some error correction for registers that don't exist.

--- read_source/read_file.py
+++ read_source/read_file.py
@@ -1,5 +1,8 @@
 # Import sys for passing arguments
 import sys
+# Import re for splitting final instruction into chunks
+import re
+
 # Import instruction dictionary for instruction lookups
 from instruction_dictionary import inst_dict
 #For reading source code file and turning into
@@ -31,6 +34,13 @@ print(inst_fields)
 
 # Extract Instruction Operator from field 0
 inst_op = inst_fields[0]
+
+# Check if 4th character of Operator specifies immediate value
+if inst_op[len(inst_op)-1] == "i":
+	immediate_operation = 1
+else:
+	immediate_operation = 0
+
 # Lookup Instruction Operator in dictionary
 inst_op_bin = inst_dict[inst_fields[0]]
 print("inst_op:", inst_op)
@@ -39,20 +49,26 @@ print("inst_op_bin:", inst_op_bin)
 
 # Extract instruction destination from field 1
 inst_dest = inst_fields[1]
+if int(inst_dest.split('$')[1]) > 31:
+	print("Register",inst_dest,"does not exist")
+	sys.exit(4)
+# Pad register location
+inst_dest_bin = "{0:b}".format(int(inst_fields[1].split('$')[1])).rjust(5, '0')
 
-# Check if the leading character is a $ or #
-if inst_dest.find("$") == 0:
-	# Destination is a register
-	#print("Destination is a register number")
-	inst_dest_bin = "{0:b}".format(int(inst_fields[1].split('$')[1])).rjust(5, '0')
-elif inst_dest.find("#") == 0:
-	# Destination is an immediate value
-	#print("Destination is an immediate value")
-	inst_dest_bin = "{0:b}".format(int(inst_fields[1].split('#')[1])).rjust(5, '0')
-else:
-	# Destination is corrupt
-	print("Destination is corrupt! Fail out!")
-	sys.exit(2)
+## Check if the leading character is a $ or #
+#if inst_dest.find("$") == 0:
+#	# Destination is a register
+#	#print("Destination is a register number")
+#	inst_dest_bin = "{0:b}".format(int(inst_fields[1].split('$')[1])).rjust(5, '0')
+#elif inst_dest.find("#") == 0:
+#	# Destination is an immediate value
+#	#print("Destination is an immediate value")
+#	inst_dest_bin = "{0:b}".format(int(inst_fields[1].split('#')[1])).rjust(5, '0')
+#else:
+#	# Destination is corrupt
+#	print("Destination is corrupt! Fail out!")
+#	sys.exit(2)
+
 # Print instruction destination
 print("inst_dest:", inst_dest)
 print("inst_dest_bin:", inst_dest_bin)
@@ -61,27 +77,29 @@ print("inst_dest_bin:", inst_dest_bin)
 
 # Extract instruction source1 from field 2
 inst_source1 = inst_fields[2]
+# Pad register location
+inst_source1_bin = "{0:b}".format(int(inst_fields[2].split('$')[1])).rjust(5, '0')
 
-# Check if the leading character is a $ or #
-if inst_source1.find("$") == 0:
-        # Source1 is a register
-        inst_source1_bin = "{0:b}".format(int(inst_fields[2].split('$')[1])).rjust(5, '0')
-elif inst_source1.find("#") == 0:
-        # Source1 is an immediate value
-        inst_source1_bin = "{0:b}".format(int(inst_fields[2].split('#')[1])).rjust(5, '0')
-else:   
-        # Source1 is corrupt
-        print("Source1 is corrupt! Fail out!")
-        sys.exit(2)
+## Check if the leading character is a $ or #
+#if inst_source1.find("$") == 0:
+#        # Source1 is a register
+#        inst_source1_bin = "{0:b}".format(int(inst_fields[2].split('$')[1])).rjust(5, '0')
+#elif inst_source1.find("#") == 0:
+#        # Source1 is an immediate value
+#        inst_source1_bin = "{0:b}".format(int(inst_fields[2].split('#')[1])).rjust(5, '0')
+#else:   
+#        # Source1 is corrupt
+#        print("Source1 is corrupt! Fail out!")
+#        sys.exit(2)
+
 # Print instruction source1
 print("inst_source1:", inst_source1)
 print("inst_source1_bin:", inst_source1_bin)
 
 
-# Extract instruction source2 from field 3
-inst_source2 = inst_fields[3]
 
-# Extract instruction source1 from field 2
+
+# Extract instruction source2 from field 3
 inst_source2 = inst_fields[3]
 
 # Check if the leading character is a $ or #
@@ -90,7 +108,7 @@ if inst_source2.find("$") == 0:
         inst_source2_bin = "{0:b}".format(int(inst_fields[3].split('$')[1])).rjust(5, '0')
 elif inst_source2.find("#") == 0:
         # Source1 is an immediate value
-        inst_source2_bin = "{0:b}".format(int(inst_fields[3].split('#')[1])).rjust(5, '0')
+        inst_source2_bin = "{0:b}".format(int(inst_fields[3].split('#')[1])).rjust(17, '0')
 else:   
         # Source1 is corrupt
         print("Source2 is corrupt! Fail out!")
@@ -102,11 +120,27 @@ print("inst_source2_bin:", inst_source2_bin)
 
 # Combine OP, Dest, Source1, and Source2 into compiled binary
 instruction_binary = inst_op_bin + inst_dest_bin + inst_source1_bin + inst_source2_bin
+instruction_binary_len = len(instruction_binary)
 
 # Force 32 bit length
 instruction_binary = instruction_binary.ljust(32, '0')
 print("Instruction Length:",len(instruction_binary))
 print("Instruction Binary:",instruction_binary)
+
+
+
+# Print formatted instruction and binary
+print("|----------------------------------------------|")
+if immediate_operation == 1:
+	print("| OP    | DEST  | S1    | Immediate Val        |")
+	print("|",inst_op_bin,"|",inst_dest_bin,"|",inst_source1_bin,"|",inst_source2_bin,"   |")
+else:
+	print("| OP    | DEST  | S1    | S2    | Unused       |")
+	print("|",inst_op_bin,"|",inst_dest_bin,"|",inst_source1_bin,"|",inst_source2_bin,"|",32-instruction_binary_len,"bits      |")
+#print("|",inst_op_bin,"|",inst_dest_bin,"|",inst_source1_bin,"|",inst_source2_bin,"|",32-instruction_binary_len,"bits     |")
+#print("|",inst_op_bin,"|",inst_dest_bin,"|",inst_source1_bin,"|",inst_source2_bin,"| 00000000000 |")
+print("|----------------------------------------------|")
+#print(re.findall('.....', instruction_binary))
 
 #inst_dest = int(inst_fields[1].split('$')[1]);
 #instruction_source = int(instruction[2].split('$')[1]);
