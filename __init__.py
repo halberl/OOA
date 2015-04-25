4cfba2527fe5cb7bab333a51f4afcacf6f67188a
Dario Cvijanovic <cvijand@fang.cs.sunyit.edu>
2015-04-25 17:24:36 -0400

    Updated init.py to read source file, convert, and write binary code to other file as a test.

--- __init__.py
+++ __init__.py
@@ -1,26 +1,38 @@
 from hw import register
 from hw import mem_collection
+from assemblyfile2bin import FileToBin
 
 NUM_OF_REG=32
 SIZE_OF_INST_MEM=2048
 SIZE_OF_DATA_MEM=32768
 
-if __name__ == "__main__":
+SOURCE_FILE = "assemblySource.txt"
+BIN_FILE = "binFile.txt"
+
+def main():
     #create registers
     data_reg=[]
 
     for it in range (0,NUM_OF_REG):
         name="r{0}".format(it)
         data_reg.append(register(name))
-
-    #for ti in range(len(data_reg)):
-        #print "Index = {0}".format(ti)
-        #print data_reg[ti].get_name()
-        #print ti
         
     #create memory
     inst_mem=mem_collection("inst", SIZE_OF_INST_MEM)
     data_mem=mem_collection("data", SIZE_OF_DATA_MEM)
 
-
     print "Done initializing mem and reg"
+
+    print "Read assembly file and convert to binary"
+
+    f = FileToBin(SOURCE_FILE, BIN_FILE)
+    print "Called FileToBin"
+    f.read()
+    print "Read source file"
+    f.write()
+
+    print "Done converting to assembly.  Next send to instruction memory"
+
+
+if __name__ == "__main__":
+    main()
---
+++ assemblyfile2bin.py
@@ -0,0 +1,70 @@
+#!/usr/local/env python3
+# -*- coding: utf-8 -*-
+'''
+Filename: assembly2Bin.py
+Author: Lucas Halbert
+Date:   04/17/15
+Modified: 04/21/15
+Description: This utility will encode assembly instructions to binary 
+             and store in the specified file.
+'''
+import sys
+from classes.encode import INSTRUCTIONEncode
+
+class FileToBin:
+    def __init__(self, sourcefile, binfile):
+        self.sourcefile = sourcefile
+        self.binfile= binfile
+        self.instructions=[]
+
+    
+    def read(self):
+         f = open(self.sourcefile, 'r')
+         with open(self.sourcefile) as f:
+             self.instructions = f.readlines()
+         
+         f.close()
+
+
+
+    def write(self):
+        # Open file for writing
+        f = open(self.binfile, 'w')
+        
+        #while :
+        for inst in self.instructions:
+            if inst== "quit":
+                # Close file to free up system resources
+                f.close
+                # Exit gracefully
+                sys.exit(0)
+        
+            # Call INSTRUCTIONEncode class
+            a=INSTRUCTIONEncode(inst)
+        
+            # Encode operation field
+            a.encodeOpField()
+        
+            # Encode destination field
+            a.encodeDestField()
+        
+            # Encode source1 field
+            a.encodeSource1Field()
+        
+            if a.immediate == 0:
+                # Encode source2 field
+                a.encodeSource2Field()
+            elif a.immediate == 1:
+                # Encode immediate value field
+                a.encodeImmediateValue()
+        
+            # Construct binary representation of instruction
+            a.constructByteCode()
+        
+            # Print constructed byte code
+            print(a.inst_bin)
+        
+            # Write byte code to file with newline
+            f.write(a.inst_bin + '\n')
+        
+        f.close()
