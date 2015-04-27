a97479b447475a90c0120fc169acc185bf41d3bc
Lucas Halbert <halberl@cs.sunyit.edu>
2015-04-27 00:43:39 -0400

    Add command line argument to specify assembly file.

--- __init__.py
+++ __init__.py
@@ -1,12 +1,16 @@
 from hw import register
 from hw import mem_collection
 from assemblyfile2bin import FileToBin
+import sys
 
 NUM_OF_REG=32
 SIZE_OF_INST_MEM=2048
 SIZE_OF_DATA_MEM=32768
 
-SOURCE_FILE = "assemblySource.txt"
+# Pull source file from command line arguments
+SOURCE_FILE = sys.argv[1]
+if SOURCE_FILE == "":
+    SOURCE_FILE = "assemblySource.txt"
 BIN_FILE = "binFile.txt"
 
 def main():
@@ -21,17 +25,17 @@ def main():
     inst_mem=mem_collection("inst", SIZE_OF_INST_MEM)
     data_mem=mem_collection("data", SIZE_OF_DATA_MEM)
 
-    print "Done initializing mem and reg"
+    print("Done initializing mem and reg")
 
-    print "Read assembly file and convert to binary"
+    print("Read assembly file and convert to binary")
 
     f = FileToBin(SOURCE_FILE, BIN_FILE)
-    print "Called FileToBin"
+    print("Called FileToBin")
     f.read()
-    print "Read source file"
+    print("Read source file")
     f.write()
 
-    print "Done converting to assembly.  Next send to instruction memory"
+    print("Done converting to assembly.  Next send to instruction memory")
 
 
 if __name__ == "__main__":
